60ff6eec58d601725c79fb15420188e47a364782
Kamil <majcherczak.kamil@protonmail.com>
2023-10-19 11:49:04 +0200

    add project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useContext, useState } from 'react';
+import { UserWarning } from './UserWarning';
+import { Todo } from './types/Todo';
+import { TodoList } from './components/TodoList';
+import { Filters } from './types/Filters';
+import { Footer } from './components/Footer';
+import { ErrorMessage } from './components/ErrorMessage';
+import { Header } from './components/Header';
+import { TodoContext } from './utils/TodoContext';
 
-function App() {
+export const App: React.FC = () => {
+  const [filterTodos, setFilterTodos] = useState<Filters>('All');
+
+  const {
+    todos,
+    USER_ID,
+    isVisibleErrorMessage,
+  } = useContext(TodoContext);
+
+  const handleFilterTodos
+  = (todosArray: Todo[], option: Filters): Todo[] => {
+    return todosArray.filter((todo) => {
+      if (option === 'Active') {
+        return !todo.completed;
+      }
+
+      if (option === 'Completed') {
+        return todo.completed;
+      }
+
+      return true;
+    });
+  };
+
+  const MadeTodoList = () => {
+    return handleFilterTodos(todos, filterTodos);
+  };
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+
+        <TodoList
+          todos={MadeTodoList()}
+        />
+
+        {todos.length !== 0 && (
+          <Footer
+            filterTodos={filterTodos}
+            setFilterTodos={setFilterTodos}
+          />
+        )}
+      </div>
+
+      {isVisibleErrorMessage && <ErrorMessage />}
+    </div>
+
+  );
+};
